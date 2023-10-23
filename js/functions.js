db623e3332c6d76b35e38e16f1be0ef76070f735
Ellona <kalesnikeha@iek.ru>
2023-10-23 14:37:12 +0300

    module5-task2

--- js/functions.js
+++ js/functions.js
@@ -3,9 +3,7 @@ const timeToMinutes = (time) => {
   return parseInt(t[0], 10) * 60 + parseInt(t[1], 10);
 };
 
-const workTime = (dayStart, dayEnd, eventStart, eventDuration) => {
-  return timeToMinutes(dayStart) <= timeToMinutes(eventStart)
+const workTime = (dayStart, dayEnd, eventStart, eventDuration) => timeToMinutes(dayStart) <= timeToMinutes(eventStart)
     && timeToMinutes(dayEnd) >= timeToMinutes(eventStart) + eventDuration;
-};
 
 workTime('14:00', '17:30', '08:0', 90);
